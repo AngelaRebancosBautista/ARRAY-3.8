b38c0b1cd1997b48806d817b5e8d98d9c9da06be
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-08-09 13:12:30 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAY_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] passwords = new string[4];
+
+            Console.WriteLine("Enter 4 passwords");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Password {i + 1}: ");
+                passwords[i] = Console.ReadLine();
+            }
+            Console.WriteLine("\nPassword Validation Results");
+            foreach (string password in passwords)
+            {
+                bool isValid = ValidatePassword(password);
+                Console.WriteLine($"Password: {password} - " + (isValid ? "Valid" : "Invalid"));
+            }
+        }
+        static bool ValidatePassword(string password)
+        {
+            if (password.Length < 6)
+            {
+                return false;
+            }
+
+            bool hasUppercase = false;
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c))
+                {
+                    hasUppercase = true;
+                    break;
+                }
+            }
+            return hasUppercase;
+        }
+    }
+}
+        
+        
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ARRAY 8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ARRAY 8")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb35799c-ca31-43d0-a09d-0e703d275ec8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
